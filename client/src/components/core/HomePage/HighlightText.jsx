322141e93cafce8d0348e41a1072d443d94d5a1b
Prince Pal <princepal262501@gmail.com>
2026-02-18 23:26:42 +0530

    created a button

--- client/src/components/core/HomePage/HighlightText.jsx
+++ client/src/components/core/HomePage/HighlightText.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HighlightText = ({text}) => {
   return (
-   <span className="font-bold bg-linear-to-r from-[#1FA2FF] via-[#12D8FA] to-[#A6FFCB] bg-clip-text text-transparent">
+   <span className="font-bold bg-linear-to-r from-[#12D8FA] via-[#12D8FA] to-[#A6FFCB] bg-clip-text text-transparent">
     {" "}
       {text}
     </span>
