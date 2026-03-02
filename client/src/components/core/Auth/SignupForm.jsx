b6f9446582c2066bfbc84569afb15c02c4192a48
Prince Pal <princepal262501@gmail.com>
2026-03-02 10:11:31 +0530

    created tab section on login and singup page

--- client/src/components/core/Auth/SignupForm.jsx
+++ client/src/components/core/Auth/SignupForm.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import {ACCOUNT_TYPE} from "../../../utils/constants"
+import Tab from '../../common/Tab'
 
 const SignupForm = () => {
+
+  const tabData = [
+    {
+      id : 1,
+      tabName : "Student",
+      type : ACCOUNT_TYPE.STUDENT
+    },
+    {
+      id : 2,
+      tabName : "Instructor",
+      type : ACCOUNT_TYPE.INSTRUCTOR
+    }
+  ]
+
   return (
     <div>
-      
+      {/* Tab  */}
+      <Tab tabData = {tabData} field/>
+
     </div>
   )
 }
