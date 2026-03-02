91dd43609adb167e56916278d49bdf70ca0bc816
Prince Pal <princepal262501@gmail.com>
2026-03-02 09:33:10 +0530

    added template component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Signup from "./pages/Signup"
 
 const App = () => {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter" >
       <Routes>
         <Route path = "/" element = {<Home/>} />
+        <Route path="signup" element = {<Signup/>} />
+        <Route path="login" element = {<Login/>} />
       </Routes>
 
     </div>
