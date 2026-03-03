30803d3b2698e22b0b5e0b227b817dca0fad7a9d
Prince Pal <princepal262501@gmail.com>
2026-03-03 12:37:16 +0530

    let's start to install and configure redux toolkit

--- client/src/components/common/Navbar.jsx
+++ client/src/components/common/Navbar.jsx
@@ -4,41 +4,47 @@ import logo from '../../assets/Logo/Logo-Full-Light.png'
 import { NavbarLinks } from '../../data/navbar-links'
 
 const Navbar = () => {
-  return (
-    <div className='flex h-14 items-center justify-center border-b border-b-richblack-700  '>
-        <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
-          {/* Image  */}
-          <Link to={"/"}>
-          <img src={logo} width={160} height={42} loading='lazy' alt="Navbar Logo" />
-          </Link>
+    return (
+        <div className='flex h-17  items-center justify-center border-b border-b-richblack-700  '>
+            <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
+                {/* Image  */}
+                <Link to={"/"}>
+                    <img src={logo} width={160} height={42} loading='lazy' alt="Navbar Logo" />
+                </Link>
 
-          {/* Nav Links */}
-          <nav>
-            <ul className='flex gap-6 text-richblack-25'>
-              {
-                NavbarLinks.map((link, idx) => (
-                    <li key={idx}>
+                {/* Nav Links */}
+                <nav>
+                    <ul className='flex gap-6 text-richblack-25'>
                         {
-                            link.title === 'Catalog' ? (<div></div>)
-                            :
-                            (<NavLink className={({isActive}) => 
-                                    isActive ? "text-yellow-25"  : " text-richblack-25 "
-                            } to={link?.path}> 
-                            <p> 
-                                {link?.title}    
-                                </p>       
-                            </NavLink>)
+                            NavbarLinks.map((link, idx) => (
+                                <li key={idx}>
+                                    {
+                                        link.title === 'Catalog' ? (<div></div>)
+                                            :
+                                            (<NavLink className={({ isActive }) =>
+                                                isActive ? "text-yellow-25" : " text-richblack-25 "
+                                            } to={link?.path}>
+                                                <p>
+                                                    {link?.title}
+                                                </p>
+                                            </NavLink>)
+                                    }
+                                </li>
+                            ))
                         }
-                    </li>
-                ))
-              }
-            </ul>
-          </nav>
+                    </ul>
+                </nav>
+
+
+                {/* Login Signup Dashboard  */}
+                <div className='flex gap-x-4 items-center'>
+
+                </div>
+
+            </div>
 
         </div>
-      
-    </div>
-  )
+    )
 }
 
 export default Navbar
