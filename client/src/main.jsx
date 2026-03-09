2ac4c8e8c4473f4c8b885062da01ac627c45397a
Prince Pal <princepal262501@gmail.com>
2026-03-09 23:46:39 +0530

    created a store and added the provider on the main

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,10 @@ import './index.css'
 import App from './App.jsx'
 import {BrowserRouter} from "react-router-dom"
 import {Provider} from 'react-redux'
+import { store } from './redux/store.js'
 
 createRoot(document.getElementById('root')).render(
-    <Provider> 
+    <Provider store={store}> 
     <BrowserRouter> 
     <App  />
     </BrowserRouter>
