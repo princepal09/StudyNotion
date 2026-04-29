a5928c0e0051ebeb0756719ef2340e1f63f40a10
Prince Pal <princepal262501@gmail.com>
2026-04-29 15:34:51 +0530

    created contact page section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,63 +1,84 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Login from "./pages/Login"
-import Signup from "./pages/Signup"
-import Navbar from "./components/common/Navbar"
-import ForgotPassword from "./pages/ForgotPassword"
-import OpenRoute from "./components/core/Auth/OpenRoute"
-import UpdatePassword from "./pages/UpdatePassword"
-import VerifyEmail from "./pages/VerifyEmail"
-import About from "./pages/About"
-import MyProfile from "./components/core/Dashboard/MyProfile"
-import Dashboard from "./pages/Dashboard"
-import PrivateRoute from "./components/core/Auth/PrivateRoute"
-import Error from "./pages/Error"
-import Settings from "./components/core/Dashboard/Settings"
-import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses"
-import Cart from "./components/core/Dashboard/Cart"
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Navbar from "./components/common/Navbar";
+import ForgotPassword from "./pages/ForgotPassword";
+import OpenRoute from "./components/core/Auth/OpenRoute";
+import UpdatePassword from "./pages/UpdatePassword";
+import VerifyEmail from "./pages/VerifyEmail";
+import About from "./pages/About";
+import MyProfile from "./components/core/Dashboard/MyProfile";
+import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/core/Auth/PrivateRoute";
+import Error from "./pages/Error";
+import Settings from "./components/core/Dashboard/Settings";
+import EnrolledCourses from "./components/core/Dashboard/EnrolledCourses";
+import Cart from "./components/core/Dashboard/Cart";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
-    <div className="w-full min-h-screen bg-richblack-900 flex flex-col font-inter" >
+    <div className="w-full min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
 
-        <Route path="signup" element={
-          <OpenRoute>
-            <Signup />
-          </OpenRoute>
-        } />
+        <Route
+          path="signup"
+          element={
+            <OpenRoute>
+              <Signup />
+            </OpenRoute>
+          }
+        />
 
-        <Route path="login" element={
-          <OpenRoute>
-            <Login />
-          </OpenRoute>
-        } />
+        <Route
+          path="login"
+          element={
+            <OpenRoute>
+              <Login />
+            </OpenRoute>
+          }
+        />
 
-        <Route path="forgot-password" element={
-          <OpenRoute>
-            <ForgotPassword />
-          </OpenRoute>
-        } />
+        <Route
+          path="forgot-password"
+          element={
+            <OpenRoute>
+              <ForgotPassword />
+            </OpenRoute>
+          }
+        />
 
-        <Route path="update-password/:id" element={
-          <OpenRoute>
-            <UpdatePassword />
-          </OpenRoute>
-        } />
+        <Route
+          path="update-password/:id"
+          element={
+            <OpenRoute>
+              <UpdatePassword />
+            </OpenRoute>
+          }
+        />
 
-        <Route path="verify-email" element={
-          <OpenRoute>
-            <VerifyEmail />
-          </OpenRoute>
-        } />
+        <Route
+          path="verify-email"
+          element={
+            <OpenRoute>
+              <VerifyEmail />
+            </OpenRoute>
+          }
+        />
 
-        <Route path="about" element={
-          <OpenRoute>
-            <About />
-          </OpenRoute>
-        } />
+        <Route
+          path="about"
+          element={
+            <OpenRoute>
+              <About />
+            </OpenRoute>
+          }
+        />
+
+        <Route path="/contact" element={<Contact />} />
 
         <Route
           element={
@@ -66,21 +87,21 @@ const App = () => {
             </PrivateRoute>
           }
         >
-
           <Route path="dashboard/my-profile" element={<MyProfile />} />
-          <Route path="dashboard/settings" element = {<Settings/>}/>
-          <Route path="dashboard/enrolled-courses" element = {<EnrolledCourses/>}/>
-          <Route path="dashboard/cart" element = {<Cart/>}/>
+          <Route path="dashboard/settings" element={<Settings />} />
+          <Route
+            path="dashboard/enrolled-courses"
+            element={<EnrolledCourses />}
+          />
+          <Route path="dashboard/cart" element={<Cart />} />
+
+          {/* TODO => for student accoun type render above two route   */}
         </Route>
 
-
         <Route path="*" element={<Error />} />
-
       </Routes>
-
     </div>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
