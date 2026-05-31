dcb5d64bbd4fc72172c0478d08d61cbb941b1c92
Prince Pal <princepal262501@gmail.com>
2026-05-31 21:56:38 +0530

    styled course sectiona and enrolled-course section

--- client/src/components/core/Course/CourseDetailsCard.jsx
+++ client/src/components/core/Course/CourseDetailsCard.jsx
@@ -4,6 +4,8 @@ import copy from 'copy-to-clipboard';
 import toast from 'react-hot-toast'
 import { ACCOUNT_TYPE } from "../../../utils/constants";
 import { addToCart } from "../../../redux/slices/cartSlice";
+import { BsFillCaretRightFill } from "react-icons/bs";
+import { FaShareSquare } from "react-icons/fa";
 const CourseDetailsCard = ({
   course,
   setConfirmationModal,
@@ -14,7 +16,11 @@ const CourseDetailsCard = ({
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const { thumbnail: thubmnailImage, price: currentPrice } = course;
+    const {
+    thumbnail: ThumbnailImage,
+    price: CurrentPrice,
+    _id: courseId,
+  } = course
 
   const handleAddToCart = () => {
     if(user && user?.accountType === ACCOUNT_TYPE.INSTRUCTOR){
@@ -42,53 +48,72 @@ const CourseDetailsCard = ({
       toast.success("Link Copied to Clipboard")
   }
   return (
-    <div>
-      <img
-        alt="Thumbnail Image"
-        className="max-h-[300px] min-h-[180px] w-[400px] rounded-xl"
-        src={thubmnailImage}
-      />
+    <>
+      <div
+        className={`flex flex-col gap-4 rounded-md bg-richblack-700 p-4 text-richblack-5`}
+      >
+        {/* Course Image */}
+        <img
+          src={ThumbnailImage}
+          alt={course?.courseName}
+          className="max-h-[300px] min-h-[180px] w-[400px] overflow-hidden rounded-2xl object-cover md:max-w-full"
+        />
 
-      <div>Rs. {currentPrice}</div>
-
-      <div>
-        <button
-          onClick={
-            user && course?.studentsEnrolled.includes(user?._id)
-              ? () => navigate("/dashboard/enrolled-courses")
-              : handleBuyCourse
-          }
-        >
-          {user && course?.studentsEnrolled.includes(user?._id)
-            ? "Go to Course"
-            : "Buy Now"}
-        </button>
-
-        {!course.studentsEnrolled.includes(user?._id) && (
-          <button onClick={handleAddToCart}>Add to Cart</button>
-        )}
-      </div>
-
-      <div>
-        <p>30 Day Money-Back Gurantee</p>
-        <p>This Course Includes :</p>
-
-        <div>
-          {course?.instructions?.map((item, idx) => (
-            <p key={idx}>
-              <span>{item}</span>
+        <div className="px-4">
+          <div className="space-x-3 pb-4 text-3xl font-semibold">
+            Rs. {CurrentPrice}
+          </div>
+          <div className="flex flex-col gap-4">
+            <button
+              className="yellowButton"
+              onClick={
+                user && course?.studentsEnrolled.includes(user?._id)
+                  ? () => navigate("/dashboard/enrolled-courses")
+                  : handleBuyCourse
+              }
+            >
+              {user && course?.studentsEnrolled.includes(user?._id)
+                ? "Go To Course"
+                : "Buy Now"}
+            </button>
+            {(!user || !course?.studentsEnrolled.includes(user?._id)) && (
+              <button onClick={handleAddToCart} className="blackButton">
+                Add to Cart
+              </button>
+            )}
+          </div>
+          <div>
+            <p className="pb-3 pt-6 text-center text-sm text-richblack-25">
+              30-Day Money-Back Guarantee
             </p>
-          ))}
+          </div>
+
+          <div className={``}>
+            <p className={`my-2 text-xl font-semibold `}>
+              This Course Includes :
+            </p>
+            <div className="flex flex-col gap-3 text-sm text-caribbeangreen-100">
+              {course?.instructions?.map((item, i) => {
+                return (
+                  <p className={`flex gap-2`} key={i}>
+                    <BsFillCaretRightFill />
+                    <span>{item}</span>
+                  </p>
+                )
+              })}
+            </div>
+          </div>
+          <div className="text-center">
+            <button
+              className="mx-auto flex items-center gap-2 py-6 text-yellow-100 "
+              onClick={handleShare}
+            >
+              <FaShareSquare size={15} /> Share
+            </button>
+          </div>
         </div>
       </div>
-
-      <div>
-        <button onClick={handleShare}>
-            Share
-        </button>
-      </div>
-
-    </div>
+    </>
   );
 };
 
--- client/src/components/core/ViewCourse/VideoDetails.jsx
+++ client/src/components/core/ViewCourse/VideoDetails.jsx
@@ -1,11 +1,164 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import ReactPlayer from "react-player";
 
 const VideoDetails = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { courseId, sectionId, subSectionId } = useParams();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const playerRef = useRef();
+  const location = useLocation();
+  const { token } = useSelector((state) => state.auth);
+  const { courseSectionData, courseEntireData, completedLectures } =
+    useSelector((state) => state.viewCourse);
 
-export default VideoDetails
+  const [videoData, setVideoData] = useState([]);
+  const [videoEnded, setVideoEnded] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const setVideoSpecificDetails = async () => {
+    if (!courseSectionData.length) return;
+
+    if (!courseId && !sectionId && !subSectionId) {
+      navigate("/dashboard/enrolled-courses");
+    } else {
+      // let's assume k all 3 fields are present
+      const filteredData = courseSectionData.filter(
+        (course) => course._id === sectionId,
+      );
+
+      const filteredVideoData = filteredData?.[0].subSection.filter(
+        (data) => data._id === subSectionId,
+      );
+
+      setVideoData(filteredVideoData[0]);
+      setVideoEnded(false);
+    }
+  };
+  useEffect(() => {
+    setVideoSpecificDetails();
+  }, []);
+
+  const isFirstVideo = () => {
+    const currentSectionIndex = courseSectionData.findIndex(
+      (data) => data._id === sectionId,
+    );
+
+    const currentSubSectionIndex = courseSectionData[
+      currentSectionIndex
+    ].subSectionId.findIndex((data) => data._id === subSectionId);
+  };
+
+  if (currentSectionIndex === 0 && currentSubSectionIndex === 0) {
+    return true;
+  } else {
+    return false;
+  }
+
+  const isLastVideo = () => {
+    const currentSectionIndex = courseSectionData.findIndex(
+      (data) => data._id === sectionId,
+    );
+
+    const noOfSubSections =
+      courseSectionData[currentSectionIndex].subSection.length;
+
+    const currentSubSectionIndex = courseSectionData[
+      currentSectionIndex
+    ].subSectionId.findIndex((data) => data._id === subSectionId);
+
+    if (
+      currentSectionIndex === courseSectionData.length - 1 &&
+      currentSubSectionIndex === noOfSubSections - 1
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  const goToNextVideo = () => {
+    const currentSectionIndex = courseSectionData.findIndex(
+      (data) => data._id === sectionId,
+    );
+
+    const noOfSubSections =
+      courseSectionData[currentSectionIndex].subSection.length;
+
+    const currentSubSectionIndex = courseSectionData[
+      currentSectionIndex
+    ].subSectionId.findIndex((data) => data._id === subSectionId);
+
+    if (currentSubSectionIndex !== noOfSubSections - 1) {
+      const nextSubSectionId =
+        courseSectionData[currentSubSectionIndex].subSection[
+          currentSectionIndex + 1
+        ]._id;
+
+      navigate(
+        `/view-course/${courseId}/section/${sectionId}/sub-section${nextSubSectionId}`,
+      );
+    } else {
+      const nextSectionId = courseSectionData[currentSectionIndex + 1]._id
+      const nextSubSectionId =
+        courseSectionData[currentSectionIndex + 1].subSection[0]._id;
+
+      navigate(
+        `/view-course/${courseId}/section/${nextSectionId}/sub-section}/${nextSubSectionId}`,
+      );
+    }
+  };
+
+  const goToPrevVideo = () => {
+    const currentSectionIndex = courseSectionData.findIndex(
+      (data) => data._id === sectionId,
+    );
+
+    const noOfSubSections =
+      courseSectionData[currentSectionIndex].subSection.length;
+
+    const currentSubSectionIndex = courseSectionData[
+      currentSectionIndex
+    ].subSectionId.findIndex((data) => data._id === subSectionId);
+
+    if (currentSubSectionIndex !== 0) {
+      // same section previous video
+      const prevSubSectionId =
+        courseSectionData[currentSectionIndex].subSection[
+          currentSubSectionIndex - 1
+        ];
+      // is video pe chle jaao
+      navigate(
+        `/view-course/${courseId}/section/${nextSectionId}/sub-section}/${prevSubSectionId}`,
+      );
+    }else{
+      // different section last video
+      const prevSectionId = courseSectionData[currentSectionIndex - 1]._id;
+      const prevSubSectionLength = courseSectionData[currentSectionIndex - 1].subSection.length;
+       const prevSubSectionId =
+        courseSectionData[currentSectionIndex - 1].subSection[prevSubSectionLength  - 1]._id;
+      navigate(
+        `/view-course/${courseId}/section/${prevSectionId}/sub-section}/${prevSubSectionId}`,
+      );
+
+    }
+  };
+
+  const handleLectureCompletion = () => { 
+    // dummy code baad m we will replace it within the actual code
+
+  };
+
+  return <div>
+    {/* {
+      !videoData ? (
+        <p>No Data Found</p>
+      ) : (
+            //  <ReactPlayer ref={playerRef} aspec >
+
+             </ReactPlayer>
+      )
+    } */}
+  </div>;
+};
+
+export default VideoDetails;
