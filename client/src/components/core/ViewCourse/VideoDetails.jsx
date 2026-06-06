a852ad8eb2df38d3d2939f315c70ec2a91b712d2
Prince Pal <princepal262501@gmail.com>
2026-06-06 12:58:25 +0530

    integrated rewatch functionality

--- client/src/components/core/ViewCourse/VideoDetails.jsx
+++ client/src/components/core/ViewCourse/VideoDetails.jsx
@@ -89,75 +89,89 @@ const VideoDetails = () => {
 
   const goToNextVideo = () => {
     const currentSectionIndex = courseSectionData.findIndex(
-        (data) => data._id === sectionId
-    )
+      (data) => data._id === sectionId,
+    );
 
-    const noOfSubSections = courseSectionData[currentSectionIndex].subSection.length;
+    const noOfSubSections =
+      courseSectionData[currentSectionIndex].subSection.length;
 
-    const currentSubSectionIndex = courseSectionData[currentSectionIndex].subSection.findIndex(
-        (data) => data._id === subSectionId
-    )
+    const currentSubSectionIndex = courseSectionData[
+      currentSectionIndex
+    ].subSection.findIndex((data) => data._id === subSectionId);
 
-    if(currentSubSectionIndex !== noOfSubSections - 1) {
-        //same section ki next video me jao
-        const nextSubSectionId = courseSectionData[currentSectionIndex].subSection[currentSectionIndex + 1]._id;
-        //next video pr jao
-        navigate(`/view-course/${courseId}/section/${sectionId}/sub-section/${nextSubSectionId}`)
+    if (currentSubSectionIndex !== noOfSubSections - 1) {
+      //same section ki next video me jao
+      const nextSubSectionId =
+        courseSectionData[currentSectionIndex].subSection[
+          currentSectionIndex + 1
+        ]._id;
+      //next video pr jao
+      navigate(
+        `/view-course/${courseId}/section/${sectionId}/sub-section/${nextSubSectionId}`,
+      );
+    } else {
+      //different section ki first video
+      const nextSectionId = courseSectionData[currentSectionIndex + 1]._id;
+      const nextSubSectionId =
+        courseSectionData[currentSectionIndex + 1].subSection[0]._id;
+      ///iss voide par jao
+      navigate(
+        `/view-course/${courseId}/section/${nextSectionId}/sub-section/${nextSubSectionId}`,
+      );
     }
-    else {
-        //different section ki first video
-        const nextSectionId = courseSectionData[currentSectionIndex + 1]._id;
-        const nextSubSectionId = courseSectionData[currentSectionIndex + 1].subSection[0]._id;
-        ///iss voide par jao 
-        navigate(`/view-course/${courseId}/section/${nextSectionId}/sub-section/${nextSubSectionId}`)
-    }
-  }
+  };
 
   const goToPrevVideo = () => {
-
     const currentSectionIndex = courseSectionData.findIndex(
-        (data) => data._id === sectionId
-    )
+      (data) => data._id === sectionId,
+    );
 
-    const noOfSubSections = courseSectionData[currentSectionIndex].subSection.length;
+    const noOfSubSections =
+      courseSectionData[currentSectionIndex].subSection.length;
 
-    const currentSubSectionIndex = courseSectionData[currentSectionIndex].subSection.findIndex(
-        (data) => data._id === subSectionId
-    )
+    const currentSubSectionIndex = courseSectionData[
+      currentSectionIndex
+    ].subSection.findIndex((data) => data._id === subSectionId);
 
-    if(currentSubSectionIndex != 0 ) {
-        //same section , prev video
-        const prevSubSectionId = courseSectionData[currentSectionIndex].subSection[currentSubSectionIndex - 1];
-        //iss video par chalge jao
-        navigate(`/view-course/${courseId}/section/${sectionId}/sub-section/${prevSubSectionId}`)
+    if (currentSubSectionIndex != 0) {
+      //same section , prev video
+      const prevSubSectionId =
+        courseSectionData[currentSectionIndex].subSection[
+          currentSubSectionIndex - 1
+        ];
+      //iss video par chalge jao
+      navigate(
+        `/view-course/${courseId}/section/${sectionId}/sub-section/${prevSubSectionId}`,
+      );
+    } else {
+      //different section , last video
+      const prevSectionId = courseSectionData[currentSectionIndex - 1]._id;
+      const prevSubSectionLength =
+        courseSectionData[currentSectionIndex - 1].subSection.length;
+      const prevSubSectionId =
+        courseSectionData[currentSectionIndex - 1].subSection[
+          prevSubSectionLength - 1
+        ]._id;
+      //iss video par chalge jao
+      navigate(
+        `/view-course/${courseId}/section/${prevSectionId}/sub-section/${prevSubSectionId}`,
+      );
     }
-    else {
-        //different section , last video
-        const prevSectionId = courseSectionData[currentSectionIndex - 1]._id;
-        const prevSubSectionLength = courseSectionData[currentSectionIndex - 1].subSection.length;
-        const prevSubSectionId = courseSectionData[currentSectionIndex - 1].subSection[prevSubSectionLength - 1]._id;
-        //iss video par chalge jao
-        navigate(`/view-course/${courseId}/section/${prevSectionId}/sub-section/${prevSubSectionId}`)
-
-    }
-
-
-  }
-
+  };
 
   const handleLectureCompletion = async () => {
-
     setLoading(true);
     //PENDING - > Course Progress PENDING
-    const res = await markLectureAsComplete({courseId: courseId, subSectionId: subSectionId}, token);
+    const res = await markLectureAsComplete(
+      { courseId: courseId, subSectionId: subSectionId },
+      token,
+    );
     //state update
-    if(res) {
-        dispatch(updateCompletedLectures(subSectionId)); 
+    if (res) {
+      dispatch(updateCompletedLectures(subSectionId));
     }
-    setLoading(false); 
-    
-   };
-  
+    setLoading(false);
+  };
 
   return (
     <div className="text-white">
@@ -170,11 +184,11 @@ const VideoDetails = () => {
             src={videoData?.videoUrl}
             controls
             config={{
-              file : {
-                attributes  :{
-                  controlList : 'nodownload'
-                }
-              }
+              file: {
+                attributes: {
+                  controlList: "nodownload",
+                },
+              },
             }}
             width="100%"
             height="500px"
@@ -195,8 +209,10 @@ const VideoDetails = () => {
                 disabled={loading}
                 onclick={() => {
                   if (playerRef?.current) {
-                    playerRef.current.seekTo(0, "seconds");
+                    playerRef.current.currentTime = 0;
+                    playerRef.current.play();
                     setVideoEnded(false);
+                    // setVideoEnded(false);
                   }
                 }}
                 text="Rewatch"
