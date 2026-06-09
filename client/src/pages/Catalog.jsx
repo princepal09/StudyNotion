1c6e7f3c690019e3293b0aca9bc4cdd92031daf5
Prince Pal <princepal262501@gmail.com>
2026-06-09 22:35:48 +0530

    catalog section styling done

--- client/src/pages/Catalog.jsx
+++ client/src/pages/Catalog.jsx
@@ -80,7 +80,7 @@ const Catalog = () => {
       </div>
 
       {/* Section 1 */}
-      <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
+      <div className=" mx-auto box-content w-full max-w-maxContentTab px-8 py-12 lg:max-w-maxContent">
         <div className="section_heading">Courses to get you started</div>
         <div className="my-4 flex border-b border-b-richblack-600 text-sm">
           <p
@@ -110,7 +110,7 @@ const Catalog = () => {
       </div>
 
       {/* Section 2 */}
-      <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
+      <div className=" mx-auto box-content w-full max-w-maxContentTab px-3 py-12 lg:max-w-maxContent">
         <div className="section_heading">
           Top courses in {catalogPageData?.differentCategory?.name}
         </div>
@@ -120,7 +120,7 @@ const Catalog = () => {
       </div>
 
       {/* Section 3 */}
-      <div className=" mx-auto box-content w-full max-w-maxContentTab px-4 py-12 lg:max-w-maxContent">
+      <div className=" mx-auto box-content w-full max-w-maxContentTab  lg:px-4 py-12 lg:max-w-maxContent">
         <div className="section_heading">Frequently Bought</div>
         <div className="py-8">
           <div className="grid grid-cols-1 gap-6 lg:grid-cols-2">
