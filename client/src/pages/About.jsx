2fa396e788dce1a21bfff005318b6b2d7348a09e
Prince Pal <princepal262501@gmail.com>
2026-06-11 23:12:32 +0530

    fixed bug on reviewSlider

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -114,10 +114,11 @@ const About = () => {
         <h1 className="text-center text-4xl font-semibold mt-8">
           Reviews from other learners
         </h1>
-        <div className="max-w-maxContent mx-auto">  
+        
+      </div>
+      <div className="max-w-maxContent mx-auto">  
         <ReviewSlider />
         </div>
-      </div>
       <Footer/>
     </div>
   )
