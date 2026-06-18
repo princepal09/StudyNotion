c64117ef3c1a22972b45cb3f27f29d8cc8db6996
Prince Pal <princepal262501@gmail.com>
2026-06-18 21:56:09 +0530

    update fn

--- server/mail/mailService.js
+++ server/mail/mailService.js
@@ -3,10 +3,13 @@ require("dotenv").config();
 
 const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
     },
+    family: 4, // Force IPv4
 })
 
 
